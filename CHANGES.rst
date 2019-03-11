2584c88c5c6570cbe74ffe9ceadc1969432e8a05
Sean P. Cornelius <spcornelius@gmail.com>
2019-03-10 20:08:56 -0400

    Prepare for v0.1.1

---
+++ CHANGES.rst
@@ -0,0 +1,7 @@
+v0.1.1
+======
+- Package housekeeping/documentation
+
+v0.1.0
+======
+- Initial release
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import setuptools
 from distutils.core import setup
 from itertools import chain
 
 pkg_name = 'paramnet'
 license = 'MIT'
-version = '0.1'
+version = '0.1.0'
 
 tests = ['paramnet.tests']
 tests_require = ['pytest>=2.9.2']
@@ -16,16 +17,36 @@ extras_req = {
 
 extras_req['all'] = list(chain(extras_req.values()))
 
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Topic :: Scientific/Engineering :: Physics"
+]
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
 if __name__ == '__main__':
     setup(
         name=pkg_name.lower(),
         version=version,
         description="NetworkX graphs with required node/edge parameters",
+        long_description=long_description,
+        long_description_content_type="text/x-rst",
         author="Sean P. Cornelius",
         author_email="spcornelius@gmail.com",
+        url="https://github.com/spcornelius/paramnet",
         license=license,
-        packages=[pkg_name],
+        packages=setuptools.find_packages(),
         install_requires=['networkx>=2.0', 'indexed.py', 'scipy'],
         tests_require=['pytest>=2.9.2', 'numpy'],
         extras_require=extras_req,
-        python_requires='>=3.6')
+        python_requires='>=3.6',
+        classifiers=classifiers)
